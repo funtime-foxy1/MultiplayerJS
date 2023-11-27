92a9de285703159f5d46e17c4d86160f9f39ba84
FunFoxRR <dldilley01@outlook.com>
2023-11-26 20:23:03 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,142 @@
+var parms = new URLSearchParams(new URL(window.location.href).searchParams);
+var _name = parms.get('name');
+
+kaboom();
+
+var loggedInUID = "";
+
+// Define openGame function
+function openGame() {
+    setBackground(50, 139, 168);
+    loadBean();
+    setGravity(1600);
+
+    const name = add([
+        text(_name),
+        pos(),
+        anchor("center")
+    ]);
+
+    const localPlayer = add([
+        sprite("bean"),
+        pos(25,25),
+        area(),
+        body(),
+        "localplr"
+    ]);
+
+    add([
+        text("UID: " + loggedInUID),
+        pos(25,25),
+        lifespan(1, {fade: .5}),
+        fadeIn(.5),
+        color(BLACK),
+        fixed(),
+        opacity(0),
+    ]);
+
+    const ground = add([
+        rect(10000, 696),
+        pos(-400, 1000),
+        area(),
+        body({isStatic: true})
+    ]);
+
+    localPlayer.onUpdate(() => {
+        tween(camPos(), localPlayer.pos, .05, (p) => camPos(p), easings.easeInOutSine);
+        name.pos = vec2(localPlayer.pos.x + (localPlayer.width/2), localPlayer.pos.y - 20);
+
+        firebase.database().ref('users/' + loggedInUID).update({
+            pos: { x: localPlayer.pos.x, y: localPlayer.pos.y }
+        })
+    });
+
+    onKeyDown("left", () => {
+        localPlayer.pos.x -= 400 * dt();
+    });
+    onKeyDown("right", () => {
+        localPlayer.pos.x += 400 * dt();
+    });
+    onKeyPress("up", () => {
+        if (localPlayer.isGrounded()) {
+            localPlayer.jump();
+        }
+    });
+}
+
+function user_listen(user) {
+    var key = user.key;
+    var childData = user.val();
+    if (key == loggedInUID) { return }
+
+    loadBean();
+
+    const name = add([
+        text(childData.username),
+        pos()
+    ]);
+
+    const plr = add([
+        sprite("bean"),
+        pos(25, 25),
+        area(),
+        "otherplr"
+    ]);
+
+    plr.onUpdate(() => {
+        name.pos = vec2(plr.pos.x - (plr.width / 2), plr.pos.y - 40);
+    });
+
+    firebase.database().ref('users/' + key + "/pos").on('value', (snap) => {
+        console.log(snap.val());
+        plr.pos = vec2(snap.val().x, snap.val().y);
+    });
+}
+
+function connecting() {
+    setBackground(BLACK);
+    add([
+        text("Connecting..."),
+        pos(center()),
+        anchor("center"),
+        color(WHITE)
+    ]);
+}
+
+scene("game", openGame);
+scene("connecting", connecting);
+
+onLoad(() => {
+    go("connecting");
+    firebase.auth().signInAnonymously()
+        .then(() => {
+            go("game");
+        }).catch(() => {
+            alert("Couldn't connect...");
+        });
+});
+
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        var uid = user.uid;
+        loggedInUID = uid;
+        firebase.database().ref('users/' + loggedInUID).set({
+            username: _name,
+            pos: { x: 0, y: 0 }
+        });
+        firebase.database().ref('users/').get().then((snapshot) => {
+            console.log(snapshot);
+            snapshot.forEach((user) => {
+                user_listen(user);
+            });
+        });
+        firebase.database().ref('users/').on('child_added', (snap) => {
+            console.log(snap);
+            user_listen(snap);
+        })
+    } else {
+        // User is signed out
+        // ...
+        alert("User is signed out");
+    }
+});
